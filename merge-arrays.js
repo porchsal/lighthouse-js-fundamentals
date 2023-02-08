c5d5a87f278d7c6159d3b374e533dc0a1bf10ce5
porchsal <porchsal@yahoo.com>
2023-02-07 22:36:24 -0500

    merge=arrays.js

---
+++ merge-arrays.js
@@ -0,0 +1,12 @@
+function merge(array1, array2){
+        var i;
+        for(i = 0; i < array2.length; i++){
+                array1.push(array2[i]);
+        }
+	array1.sort();
+        return array1;
+}
+
+console.log(merge([ 4, 5, 6 ], [ 1, 2, 3, 4 ]), "=?", [ 1, 2, 3, 4, 4, 5, 6 ]);
+console.log(merge([ 4 ], [ 2, 5, 8 ]), "=?", [ 2, 4, 5, 8 ]);
+console.log(merge([ 1, 2, 6 ], []), "=?", [ 1, 2, 6 ]);
